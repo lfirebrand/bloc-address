d924c4b9a13ebdb97aee9e2f9b245d4d0a14d15c
Lexi Zgud <lexibitesblog@gmail.com>
2018-09-18 23:05:04 -0500

    Assignment Contacts-Database - Submitted

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -46,14 +46,14 @@ module.exports = class MenuController {
   addContact(){
     this.clear();
     inquirer.prompt(this.book.addContactQuestions).then((answers) => {
-      this.book.addContact(answers.name, answers.phone).then((contact) => {
+      this.book.addContact(answers.name, answers.phone, answers.email).then((contact) => {
         console.log("Contact added successfully!");
         this.main();
       }).catch((err) => {
         console.log(err);
         this.main();
   });
-});  
+});
     this.main();
   }
 
