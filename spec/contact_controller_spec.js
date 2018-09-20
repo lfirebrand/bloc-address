8e61dc1a8a854bbfb63db9a99789364717a47528
Lexi Zgud <lexibitesblog@gmail.com>
2018-09-19 20:41:01 -0500

    committing to get help

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -19,11 +19,11 @@ describe('ContactController', () => {
 	describe('#addContact()', () => {
 		it('should add a single contact into the book', done => {
 			this.book
-				.addContact('Alice', '001-101-1010', 'alice.smith@gmail.com')
+				.addContact('Alice', '001-101-1010', 'alice@example.com')
 				.then(contact => {
 					expect(contact.name).toBe('Alice');
 					expect(contact.phone).toBe('001-101-1010');
-					expect(contact.email).toBe('alice.smith@gmail.com');
+					expect(contact.email).toBe('alice@example.com');
 					done();
 				})
 				.catch(err => {
@@ -31,4 +31,35 @@ describe('ContactController', () => {
 				});
 		});
 	});
+});
+
+describe("#getContacts()", () => {
+
+	it("should return an empty array when no contacts are available", (done) => {
+		this.book.getContacts()
+			.then((contacts) => {
+				expect(contacts.length).toBe(0);
+				done();
+			})
+			.catch((err) => {
+				console.log(err);
+				done();
+			});
+	});
+
+	it("should return an array of contacts when contacts are available", (done) => {
+		this.book.addContact("Alice", "001-101-1010", "alice@example.com")
+			.then(() => {
+				this.book.getContacts()
+					.then((contacts) => {
+						expect(contacts.length).toBe(1);
+						done();
+					});
+			})
+			.catch((err) => {
+				console.log(err);
+				done();
+			});
+	});
+
 });
